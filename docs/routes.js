2e248e2658d94b2bb4b1318bc29da33079887730
Batis <18264315@qq.com>
2022-06-15 16:16:32 +0800

    Update routes.js

--- docs/routes.js
+++ docs/routes.js
@@ -1,3 +1,6 @@
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+// 上面是测试
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
